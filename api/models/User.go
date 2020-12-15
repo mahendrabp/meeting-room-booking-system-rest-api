6b0b051ad215b6714f70905f15b4143c487f8a3b
mahendrabp <chipxitro@gmail.com>
2020-12-15 17:27:59 +0700

    Add: SaveUser method, and FindByUserId method in model User

--- api/models/User.go
+++ api/models/User.go
@@ -1,7 +1,11 @@
 package models
 
 import (
+	"errors"
+	"github.com/jinzhu/gorm"
 	"github.com/mahendrabp/meeting-room-booking-system-rest-api/api/helpers"
+	"html"
+	"strings"
 	"time"
 )
 
@@ -23,4 +27,32 @@ func (u *User) BeforeSave() error {
 	}
 	u.Password = string(hashedPassword)
 	return nil
+}
+
+func (u *User) Prepare() {
+	u.Email = html.EscapeString(strings.TrimSpace(u.Email))
+	u.Role = "guest"
+	u.CreatedAt = time.Now()
+	u.UpdatedAt = time.Now()
+}
+
+func (u *User) SaveUser(db *gorm.DB) (*User, error) {
+	var err error
+	err = db.Debug().Create(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return u, nil
+}
+
+func (u *User) FindUserByID(db *gorm.DB, uid uint32) (*User, error) {
+	var err error
+	err = db.Debug().Model(User{}).Where("id = ?", uid).Take(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	if gorm.IsRecordNotFoundError(err) {
+		return &User{}, errors.New("User Not Found")
+	}
+	return u, err
 }
