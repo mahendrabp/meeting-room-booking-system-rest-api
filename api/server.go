e8010230465925a5760cd848003ed947be04f731
mahendrabp <chipxitro@gmail.com>
2020-12-17 10:42:23 +0700

    Change: PORT to os.Getenv(PORT)

--- api/server.go
+++ api/server.go
@@ -30,8 +30,8 @@ func Run() {
 	// This is for testing, when done, do well to comment
 	//seeder.Load(server.DB)
 
-	PORT := "8202"
-	apiPort := fmt.Sprintf(":%s", PORT)
+	//PORT := "8202"
+	apiPort := fmt.Sprintf(":%s", os.Getenv("PORT"))
 	fmt.Printf("Listening to port %s", apiPort)
 	server.Run(apiPort)
 
